357172b4bbe16ee3201585d7d93016a3c0be14f4
TxemaN <txalcala@hotmail.com>
2023-08-09 12:04:55 +0200

    probando render

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const {dbConnect} = require("./helpers/connection")
+const cors = require ('cors')
 require('dotenv').config();
 
 
@@ -8,10 +9,13 @@ require('dotenv').config();
 const app=express();
 
 //PUERTO
-const port = process.env.port
+const port = process.env.port || 3000
 
 //CONEXIÓN A BASE
 dbConnect()
+
+//CORS
+app.use(cors())
 
 //parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }))
