44bf49991252be3f96cdaccfc5d24b032f331ef3
TxemaN <txalcala@hotmail.com>
2023-08-13 16:21:02 +0200

    datos binarios imagenes

--- models/BlogModel.js
+++ models/BlogModel.js
@@ -1,27 +1,30 @@
 const { Schema, model } = require("mongoose");
-
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const blogSchema = new Schema({
-titulo: {
-type: String,
-require: true,
+    titulo: {
+        type: String,
+        require: true,
 
-},
-noticia: {
-type: String,
-require: true,
+    },
+    noticia: {
+        type: String,
+        require: true,
 
-},
-imagen: {
-    type: String,
-require: true,
-  
-    
+    },
+    imagen: {
+        type: Buffer,
+        data: String
+
+        
+
+
     },
 
-fecha: {
-    type: Date,
-    default: Date.now
-}
+    fecha: {
+        type: Date,
+        default: Date.now
+    }
 
 
 
