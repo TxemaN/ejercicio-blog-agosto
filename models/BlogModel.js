341c96c284cdb107c241dcf2d96a41441f209625
TxemaN <txalcala@hotmail.com>
2023-09-06 18:33:10 +0200

    string

--- models/BlogModel.js
+++ models/BlogModel.js
@@ -12,7 +12,13 @@ const blogSchema = new Schema({
         require: true,
 
     },
-    
+    imagensrc: {
+        type: String,
+
+    },
+    imagenalt: {
+        type: String,
+    },
     fecha: {
         type: Date,
         default: Date.now
@@ -28,9 +34,7 @@ const blogSchema = new Schema({
 
     },
     uploaded_file: {
-        data: Buffer,
-        contentType: String,
-        nombreArchivo: String // Campo adicional para el nombre del archivo
+        type: String,
       }
 
 
