119cb3db9bf9085a22a96c2ab48d0e7e53f8b31b
TxemaN <txalcala@hotmail.com>
2023-09-07 18:48:26 +0200

    security true out

--- controllers/controllerAuth.js
+++ controllers/controllerAuth.js
@@ -172,7 +172,7 @@ const loginUser = async (req, res,) => {
         const token = await generarJWT(user.id, user.nombre, user.role);
         res.cookie('miToken', token, {
             httpOnly: true,
-            secure: true, // Somente se o frontend estiver sendo servido por HTTPS
+           
             sameSite: 'none', // Envia cookei a servidores distintos
             maxAge: 12 * 60 * 60 * 1000, //12h
 
